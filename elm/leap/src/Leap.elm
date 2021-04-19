f68728d8f7c3eb3e78ef808b36ef192c19f06d41
Steven Pequeno <steven@pequeno.in>
2021-04-19 09:29:46 -0500

    simplify solution for leap again

--- elm/leap/src/Leap.elm
+++ elm/leap/src/Leap.elm
@@ -3,14 +3,11 @@ module Leap exposing (isLeapYear)
 
 isLeapYear : Int -> Bool
 isLeapYear year =
-    if isDivisibleBy 4 year then
-        not
+    isDivisibleBy 4 year
+        && not
             (isDivisibleBy 100 year
                 && not (isDivisibleBy 400 year)
             )
-
-    else
-        False
 
 
 isDivisibleBy divisor n =
