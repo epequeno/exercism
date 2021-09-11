7f6239f6dc1335d28ddebcf47fb495d9d4c4b3d1
Steven Pequeno <steven@pequeno.in>
2021-09-11 11:21:49 -0500

    more explicit tuple struct access

--- rust/high-scores/src/lib.rs
+++ rust/high-scores/src/lib.rs
@@ -7,23 +7,19 @@ impl HighScores {
     }
 
     pub fn scores(&self) -> &[u32] {
-        let HighScores(s) = self;
-        s.as_slice()
+        self.0.as_slice()
     }
 
     pub fn latest(&self) -> Option<u32> {
-        let HighScores(s) = self;
-        s.iter().cloned().last()
+        self.0.iter().cloned().last()
     }
 
     pub fn personal_best(&self) -> Option<u32> {
-        let HighScores(s) = self;
-        s.iter().cloned().max()
+        self.0.iter().cloned().max()
     }
 
     pub fn personal_top_three(&self) -> Vec<u32> {
-        let HighScores(s) = self;
-        let mut res = s.iter().cloned().collect::<Vec<u32>>();
+        let mut res = self.0.iter().cloned().collect::<Vec<u32>>();
         res.sort();
         res.reverse();
         res.into_iter().take(3).collect()
